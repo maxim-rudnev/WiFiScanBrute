576a259a1e1ac29503f186f48cf8b3e06cb096c4
admin <admin@DESKTOP-SMCU6BU>
2021-02-27 19:34:30 +0300

    initial

---
+++ WiFiScanBrute/UI.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WiFiScanBrute
+{
+    partial class UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView_networks = new System.Windows.Forms.DataGridView();
+            this.SSID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AuthAlgo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SignalQuality = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button_scan = new System.Windows.Forms.Button();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.label_status = new System.Windows.Forms.Label();
+            this.button_disconnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_networks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_networks
+            // 
+            this.dataGridView_networks.AllowUserToAddRows = false;
+            this.dataGridView_networks.AllowUserToDeleteRows = false;
+            this.dataGridView_networks.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView_networks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_networks.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SSID,
+            this.AuthAlgo,
+            this.SignalQuality});
+            this.dataGridView_networks.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView_networks.MultiSelect = false;
+            this.dataGridView_networks.Name = "dataGridView_networks";
+            this.dataGridView_networks.RowHeadersVisible = false;
+            this.dataGridView_networks.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView_networks.Size = new System.Drawing.Size(507, 404);
+            this.dataGridView_networks.TabIndex = 0;
+            // 
+            // SSID
+            // 
+            this.SSID.HeaderText = "SSID";
+            this.SSID.Name = "SSID";
+            this.SSID.ReadOnly = true;
+            this.SSID.Width = 300;
+            // 
+            // AuthAlgo
+            // 
+            this.AuthAlgo.HeaderText = "AuthType";
+            this.AuthAlgo.Name = "AuthAlgo";
+            this.AuthAlgo.ReadOnly = true;
+            // 
+            // SignalQuality
+            // 
+            this.SignalQuality.HeaderText = "SignalQuality";
+            this.SignalQuality.Name = "SignalQuality";
+            // 
+            // button_scan
+            // 
+            this.button_scan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_scan.Location = new System.Drawing.Point(525, 12);
+            this.button_scan.Name = "button_scan";
+            this.button_scan.Size = new System.Drawing.Size(232, 23);
+            this.button_scan.TabIndex = 1;
+            this.button_scan.Text = "Сканировать";
+            this.button_scan.UseVisualStyleBackColor = true;
+            this.button_scan.Click += new System.EventHandler(this.button_scan_Click);
+            // 
+            // button_connect
+            // 
+            this.button_connect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_connect.Location = new System.Drawing.Point(525, 91);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(232, 23);
+            this.button_connect.TabIndex = 2;
+            this.button_connect.Text = "Подключиться";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_password.Location = new System.Drawing.Point(526, 63);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(231, 20);
+            this.textBox_password.TabIndex = 3;
+            // 
+            // label_status
+            // 
+            this.label_status.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_status.AutoSize = true;
+            this.label_status.Location = new System.Drawing.Point(525, 153);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(43, 13);
+            this.label_status.TabIndex = 4;
+            this.label_status.Text = "Status: ";
+            // 
+            // button_disconnect
+            // 
+            this.button_disconnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_disconnect.Location = new System.Drawing.Point(525, 120);
+            this.button_disconnect.Name = "button_disconnect";
+            this.button_disconnect.Size = new System.Drawing.Size(232, 23);
+            this.button_disconnect.TabIndex = 5;
+            this.button_disconnect.Text = "Отключиться";
+            this.button_disconnect.UseVisualStyleBackColor = true;
+            this.button_disconnect.Click += new System.EventHandler(this.button_disconnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(525, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Password:";
+            // 
+            // UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_disconnect);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.button_scan);
+            this.Controls.Add(this.dataGridView_networks);
+            this.Name = "UI";
+            this.Text = "WiFi Scan Brute";
+            this.Load += new System.EventHandler(this.UI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_networks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_networks;
+        private System.Windows.Forms.Button button_scan;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Button button_disconnect;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SSID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AuthAlgo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SignalQuality;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WiFiScanBrute/UI.cs
@@ -0,0 +1,145 @@
+﻿using SimpleWifi;
+using SimpleWifi.Win32;
+using SimpleWifi.Win32.Interop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WiFiScanBrute
+{
+
+    /// <summary>
+    /// реализовано на базе simplewifi
+    /// https://www.nuget.org/packages/SimpleWifi
+    /// https://github.com/DigiExam/simplewifi
+    /// Алгоритмы аутентификации
+    /// https://github.com/emoacht/ManagedNativeWifi/blob/master/Source/ManagedNativeWifi/AuthenticationAlgorithm.cs
+    /// </summary>
+    public partial class UI : Form
+    {
+        Wifi _wifiConnectClient = new Wifi();           // используется при подключения к сети
+        WlanClient _wifiScanClient = new WlanClient();  // используется при сканировании сетей
+
+        List<WlanAvailableNetwork> _wifiNetworks = new List<WlanAvailableNetwork>();
+
+        public UI()
+        {
+            InitializeComponent();
+        }
+
+        private void UI_Load(object sender, EventArgs e)
+        {
+            _ui_update_connect_state();
+        }
+
+        private void button_scan_Click(object sender, EventArgs e)
+        {
+            if (_wifiConnectClient.ConnectionStatus == WifiStatus.Connected)
+            {
+                MessageBox.Show("Сканирование доступно только при отключении от сетей WiFi");
+                return;
+            }
+
+            // сканирование сети и получение списка доступных WiFi сетей
+            _wifiNetworks = _wifiScanClient.Interfaces[0].GetAvailableNetworkList(0).ToList();
+
+            // формирование модели представления списка доступных сетей
+            var netListVM = _wifiNetworks.Select(x => 
+                                                    new { 
+                                                        SSID = _getStringForSSID(x.dot11Ssid), 
+                                                        AuthAlgo = x.dot11DefaultAuthAlgorithm,
+                                                        SignalQuality = x.wlanSignalQuality.ToString() + "%"
+                                                    })
+                                            .OrderBy(x=>x.SSID)
+                                            .ToList();
+
+            // загрузка данных модели представления в UI
+            dataGridView_networks.Rows.Clear();
+            netListVM.ForEach(x =>
+                                {
+                                    dataGridView_networks.Rows.Add(new object[] { x.SSID, x.AuthAlgo, x.SignalQuality });
+                                });
+
+            dataGridView_networks.Invalidate();
+        }
+
+        static string _getStringForSSID(Dot11Ssid ssid)
+        {
+            return Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
+        }
+
+        private void button_disconnect_Click(object sender, EventArgs e)
+        {
+            _wifiConnectClient.Disconnect();
+
+            _ui_update_connect_state(false);
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            string selectedNetwork = null;
+            
+            // проверка, выбрана ли сеть для подключения
+            if (dataGridView_networks.SelectedRows.Count > 0)
+                selectedNetwork = (string)dataGridView_networks.SelectedRows[0].Cells["SSID"].Value;
+
+            if (string.IsNullOrEmpty(selectedNetwork.Trim()))
+            {
+                MessageBox.Show($"Не выбрана сеть для подключения");
+                return;
+            }
+
+
+            List<AccessPoint> aps = _wifiConnectClient.GetAccessPoints();
+            AccessPoint ap = aps.Where(x => x.Name == selectedNetwork).FirstOrDefault();
+
+            AuthRequest ar = new AuthRequest(ap);
+            ar.Password = textBox_password.Text;
+
+            bool res = ap.Connect(ar, true);
+
+            _ui_update_connect_state(res);
+
+            if (!res)            
+            {                
+                MessageBox.Show($"Подключение к сети {selectedNetwork} - не установлено!");
+            }
+            
+        }
+
+        
+
+        private void _ui_update_connect_state(bool? connected = null)
+        {
+            if ( connected == null && _wifiScanClient.Interfaces[0].InterfaceState == WlanInterfaceState.Connected)
+            {
+                label_status.Text = "Status: connected to " + _getCurrentNetworkName();
+            }
+            else if (connected != null && connected.Value)
+            {
+                label_status.Text = "Status: connected to " + _getCurrentNetworkName();
+            }
+            else
+                label_status.Text = "Status: disconnected";
+        }
+
+        private string _getCurrentNetworkName()
+        {
+            string res = string.Empty;
+
+            try
+            {
+                res = _wifiScanClient.Interfaces[0].CurrentConnection.profileName;
+            }
+            catch { }
+
+            return res;
+        }
+    }
+}
